3de76d6dcb631af7327d8cd1088f35bc73880dd0
Davi Aires <daviaires10@hotmail.com>
2020-12-18 16:53:19 +0000

    Boa

--- main.py
+++ main.py
@@ -4,7 +4,13 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
+    x = float(input('Digite um número.'))
+    y = float(input('Digite outro número.'))
+    z = float(input('Digite mais um número.'))
+    print(f'{max(x,y,z)}')
 
 
+
+    
 if __name__ == '__main__':
     main()
